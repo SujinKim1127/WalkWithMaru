cd61e787684140e5d87a17c1404f2266d0590292
SuJin <kimsujin001127@gmail.com>
2023-03-26 14:27:40 +0900

    ✨ Time part UI 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Calendar from "./Calendar/Calendar";
+import Member from "./components/Member";
+import Part from "./components/Part";
 
 function App() {
   return (
     <div className="App">
       <Calendar />
+      <Part />
+      <Member />
     </div>
   );
 }
---
+++ src/components/Part.tsx
@@ -0,0 +1,99 @@
+import styled from "styled-components";
+import color from "../util/color";
+import { Icon } from "@iconify/react";
+
+const { green, sky, purple, yellow, pink, brown } = color;
+
+const Part = () => {
+  return (
+    <Container>
+      <MorningBox>
+        <MorningText>
+          <TextBox>아침</TextBox>
+        </MorningText>
+        <MorningPartBox>
+          <UserBox>
+            <UserNametxt>수진&nbsp;</UserNametxt>
+            <UserDeletebtn>
+              <Icon icon="ic:round-close" />
+            </UserDeletebtn>
+          </UserBox>
+        </MorningPartBox>
+      </MorningBox>
+      <EveningBox>
+        <EveningText>
+          <TextBox>저녁</TextBox>
+        </EveningText>
+        <EveningPartBox>
+          <UserBox>
+            <UserNametxt>수진&nbsp;</UserNametxt>
+            <UserDeletebtn>
+              <Icon icon="ic:round-close" />
+            </UserDeletebtn>
+          </UserBox>
+        </EveningPartBox>
+      </EveningBox>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin: 10px auto;
+  width: 350px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const TextBox = styled.div`
+  margin: auto;
+`;
+
+const MorningBox = styled.div`
+  display: flex;
+  height: 75px;
+`;
+
+const MorningText = styled.div`
+  display: flex;
+  text-align: center;
+  width: 30px;
+`;
+
+const MorningPartBox = styled.div`
+  margin: auto 0 auto 15px;
+`;
+
+const EveningBox = styled.div`
+  display: flex;
+  height: 75px;
+`;
+
+const EveningText = styled.div`
+  display: flex;
+  text-align: center;
+  width: 30px;
+`;
+
+const EveningPartBox = styled.div`
+  margin: auto 0 auto 15px;
+`;
+
+const UserBox = styled.div`
+  width: 60px;
+  height: 32px;
+  display: flex;
+  background-color: ${yellow};
+  border-radius: 10px;
+`;
+
+const UserNametxt = styled.div`
+  margin: auto 0 auto 7px;
+`;
+
+const UserDeletebtn = styled.div`
+  display: flex;
+  margin: auto 0;
+`;
+
+export default Part;
