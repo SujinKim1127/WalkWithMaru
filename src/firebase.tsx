08aea1ab810a4f4c5997c8d91ad5093d9d3ba0da
SuJin <kimsujin001127@gmail.com>
2023-04-03 18:59:19 +0900

    🐛 fix: firebase 수정

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore"; // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,4 +13,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
+export const firebaseInstance = firebase;
+export const dbService = firebase.firestore();
